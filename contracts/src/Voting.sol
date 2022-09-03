2e8ab554d61b66760c78acf2afbd3b28b07cf4f1
Murilo <muriloolegini@outlook.com>
2022-09-03 18:38:22 -0300

    First Commit

---
+++ contracts/src/Voting.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.16;
+
+contract Voting {
+
+    mapping (string => uint256) public votesReceived;
+    
+    string[] public candidateList;
+
+    constructor(string[] memory candidateName) {
+        candidateList = candidateName;
+    }
+
+    function totalVotesFor(string memory candidate) view public returns (uint256) {
+
+        require(validCandidate(candidate));
+
+        return votesReceived[candidate];
+    }
+
+    function voteForCandidate(string memory candidate) public {
+
+        require(validCandidate(candidate));
+        votesReceived[candidate] +=1;
+    }
+
+    function validCandidate(string memory candidate) view public returns (bool) {
+        for (uint i = 0; i < candidateList.length; i++) {
+            if (keccak256(abi.encodePacked(candidateList[i])) == keccak256(abi.encodePacked(candidate))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
